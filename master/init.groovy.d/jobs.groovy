f7a43e25d78d77f31b887a5c3e8be3ff86d8ebb1
Ofer Sadan <ofersadan85@gmail.com>
2023-01-03 02:02:12 +0200

    multi branch job

--- master/init.groovy.d/jobs.groovy
+++ master/init.groovy.d/jobs.groovy
@@ -1,12 +1,30 @@
 import jenkins.model.Jenkins
-import hudson.plugins.git.GitSCM
-import hudson.plugins.git.BranchSpec
-import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
-import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import jenkins.branch.BranchSource
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+import com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger
 
-def scm = new GitSCM('https://github.com/ofersadan85/devops-demo')
-scm.branches = [new BranchSpec('refs/heads/main')]
+// I don't like this wildcard import, but it's the only way to get the traits for some reason
+import org.jenkinsci.plugins.github_branch_source.*
+
 jenkins = Jenkins.instance
-job = new WorkflowJob(jenkins, 'example-app-python')
-job.definition = new CpsScmFlowDefinition(scm, 'example-app-python/Jenkinsfile')
+job = new WorkflowMultiBranchProject(jenkins, 'example-app-python')
+job.addTrigger(new PeriodicFolderTrigger('3m'))
+github = new GitHubSCMSource(
+    'ofersadan85',
+    'devops-demo',
+    'https://github.com/ofersadan85/devops-demo',
+    true
+)
+traits = [
+    new BranchDiscoveryTrait(BranchDiscoveryTrait.ALL_BRANCHES),
+    new OriginPullRequestDiscoveryTrait(OriginPullRequestDiscoveryTrait.MERGE),
+    new ForkPullRequestDiscoveryTrait(
+        ForkPullRequestDiscoveryTrait.MERGE,
+        new ForkPullRequestDiscoveryTrait.TrustNobody()
+    )
+]
+github.setTraits(traits)
+job.getProjectFactory().setScriptPath('example-app-python/Jenkinsfile')
+job.getSourcesList().add(new BranchSource(github))
+
 jenkins.reload()
